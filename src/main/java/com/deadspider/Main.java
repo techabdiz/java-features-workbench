f3ffdb66a1fe617124fc16e05dcb035187e726fd
Your Name <you@example.com>
2024-08-07 17:47:11 +0530

    inital commit, this repository will store all java feature worked files

---
+++ src/main/java/com/deadspider/Main.java
@@ -0,0 +1,31 @@
+package com.deadspider;
+
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BiPredicate<String, String> s = (a,b)->a.startsWith(b);
+
+        System.out.println(s.test("hello", "h"));
+
+
+        Predicate<String> p =
+                ((Predicate<String>) str-> str.startsWith("h"))
+                .and(str->str.split(" ").length > 2)
+                .or(str->str.contains("test")).or(Predicate.not(str->str.contains("string")));
+
+        System.out.println(p.test("h das string"));
+
+    }
+
+    public static <T> boolean check(T t, Predicate<T> p) {
+        return p.test(t);
+    }
+
+    public static <T> boolean check(T t, Predicate<T> p,
+                                    Predicate<T> q) {
+        return p.and(p).test(t);
+    }
+}

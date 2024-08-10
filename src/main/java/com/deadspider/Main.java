c6653093fb4a6d666641707538ef8f8ba31dde0b
Your Name <you@example.com>
2024-08-11 00:50:22 +0530

    functional supplier/consumer in MainRunner

--- src/main/java/com/deadspider/Main.java
+++ src/main/java/com/deadspider/Main.java
@@ -12,33 +12,43 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.BiPredicate;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
+
+    public static final String LINE_DELEMTER = "\\ ";
+
+    public static Function<String, Stream<String>> delimitString =
+                (string)->Arrays.asList(string.split(LINE_DELEMTER)).stream();
+
+    public static Function<Stream<String>, List<String>> flatMapList =
+                (s)-> s.flatMap(delimitString).toList();
 
     public static Function<String, Optional<List<String>>> getUrlDataInWords = (url)->{
         try {
             URL uri = new URL(url);
-            return Optional.ofNullable(new BufferedReader(new InputStreamReader(uri.openConnection().getInputStream()))
-                    .lines().flatMap(s-> Arrays.asList(s.split("\\ ")).stream())
-                            .filter(s->!s.isBlank())
-                    .toList());
+            return Optional.ofNullable(flatMapList.apply(new BufferedReader(new InputStreamReader(uri.openConnection().getInputStream())).lines()));
         } catch (IOException e) {
         }
         return Optional.empty();
     };
 
+    public static Supplier<Stream<String>> getData = ()-> getUrlDataInWords.apply("https://www.ietf.org/rfc/rfc2616.txt").get().stream();
+
+
 
     public static void main(String[] args) {
         PageManager<String> pageman = new SimplePageManager<>();
 
-        getUrlDataInWords.apply("https://www.ietf.org/rfc/rfc2616.txt")
-                        .ifPresent(s-> s.forEach(pageman::add));
+        Consumer<String> consumer = pageman::add;
+        Consumer<Stream<String>> streamConsumer = s -> s.forEach(consumer::accept);
+        streamConsumer.accept(getData.get());
 
+
+        System.out.println(pageman.has("aziz"));
+        System.out.println(pageman.has("http"));
         System.out.println(pageman);
     }
 }
